71c3dfb33ea34f801ecab90898c5777b6b886382
sinan-894 <mohammedsinan1011@gmail.com>
2025-05-14 23:40:51 +0530

    Setup the structure  header of the website using DOM

---
+++ src/dom.js
@@ -0,0 +1,5 @@
+
+
+const headerGroup  = (()=>{const header = document.createElement('header');
+const nav = document.createElement("nav");
+const headNav = document.createElement('button')})
--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 
 
 
-console.log("hello world");
+
+const header = document.createElement('header');
+const nav = document.createElement("nav");
+const navButtonsArray = ['Head','Menu','About'];
+navButtonsArray.forEach((buttonName)=>{
+    let navButton = document.createElement('button');
+    nav.appendChild(navButton);
+    navButton.id = buttonName
+    navButton.textContent = buttonName
+    navButton.classList.add(buttonName,'nav-button')
+})
+header.appendChild(nav)
+document.body.appendChild(header)
