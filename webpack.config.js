9b1000fc4b1b62cdb6e2b7197d7757cc405511f8
sinan-894 <mohammedsinan1011@gmail.com>
2025-05-14 22:51:47 +0530

    Initialize webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+// webpack.config.js
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  }
+};
